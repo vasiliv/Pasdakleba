d317a68030803d14a9d415e48df3aa4632f364f6
vasiliv <32122468+vasiliv@users.noreply.github.com>
2025-03-04 23:13:37 +0400

    v2 - Add Entity classes Sale and Brand

---
+++ Pasdakleba.Domain/Entities/Brand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pasdakleba.Domain.Entities
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public required string NameGeo { get; set; }
+        public required string NameEng { get; set; }
+
+        //Navigation properties
+        public int SaleId { get; set; }
+        public Sale Sale { get; set; }
+    }
+}
---
+++ Pasdakleba.Domain/Entities/Sale.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pasdakleba.Domain.Entities
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        //Navigation properties
+        public ICollection<Brand> Brands { get; set; } = new List<Brand>();
+    }
+}
