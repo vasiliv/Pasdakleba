4f4b0cf5148fc00c00a030d34fe4227b361abf33
vasiliv <32122468+vasiliv@users.noreply.github.com>
2025-03-04 22:29:00 +0400

    Add project files.

---
+++ Pasdakleba.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pasdakleba.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
