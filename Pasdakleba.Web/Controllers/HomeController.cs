c57dab482ac5d76008c2641f8e2b204149c6ff15
vasiliv <32122468+vasiliv@users.noreply.github.com>
2025-03-05 17:23:24 +0400

    v6 - HomeController

--- Pasdakleba.Web/Controllers/HomeController.cs
+++ Pasdakleba.Web/Controllers/HomeController.cs
@@ -1,31 +1,20 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Pasdakleba.Infrastructure.Data;
 using Pasdakleba.Web.Models;
 
 namespace Pasdakleba.Web.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    private readonly ApplicationDbContext _db;
+    public HomeController(ApplicationDbContext db)
     {
-        _logger = logger;
+        _db = db;
     }
-
     public IActionResult Index()
     {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+        var Brands = _db.Brands.ToList();
+        return View(Brands);
+    }    
 }
