670ca4e8ea64d44c238264591ad5f504d786ffdd
vasiliv <32122468+vasiliv@users.noreply.github.com>
2025-03-11 15:04:06 +0400

    v28 - ViewModels

---
+++ Pasdakleba.Web/ViewModels/SaleVM.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Pasdakleba.Domain.Entities;
+
+namespace Pasdakleba.Web.ViewModels
+{
+    public class SaleVM
+    {
+        public Sale? Sale { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> BrandList { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> SaleTypeList { get; set; }
+    }
+}
